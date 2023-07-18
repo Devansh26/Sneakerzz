efc75cdad597905c3f04c1630a7eb9eacd903f49
Dharmil Chhadva <dharmilch18@gmail.com>
2023-07-17 22:59:08 -0400

    Added login page & validation. Changed file names

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
 function validateForm() {
 
-    event.preventDefault();
+  event.preventDefault();
 
     // Perform form validation and display errors
   const firstName = document.getElementById('first_name').value;
@@ -48,19 +48,48 @@ function validateForm() {
   if (!checkBox.checked) {
     document.getElementById("checkbox_error").textContent = 'You must accept the terms and conditions by checking the Checkbox';
     isValid = false;
-    console.log("in check box if")
-    console.log("Checkbox?: ", checkBox.checked);
   } else {
-    console.log("Checkbox?: ", checkBox.checked);
-    console.log("in check box else")
     document.getElementById("checkbox_error").textContent = '';
   }
 
-
-  console.log("Valid?: " + isValid)
   if (isValid) {
     // Redirect to login page or perform any other action
     window.location.href = 'login.html';
   }
 }
+
+function validateLogin() {
+  event.preventDefault();
+
+    // Perform form validation and display errors
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('pwd').value;
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+
+  let isValid = true;
+
+  if (!emailRegex.test(email)) {
+    document.getElementById('email_error').textContent = 'Please enter a valid email address.';
+    isValid = false;
+  } else {
+    document.getElementById('email_error').textContent = '';
+  }
+
+  if (!passwordRegex.test(password)) {
+    document.getElementById('pwd_error').textContent = 'Password must be at least 8 characters long and contain at least one number, one lowercase letter, and one uppercase letter.';
+    isValid = false;
+  }
+  else {
+    document.getElementById('pwd_error').textContent = '';
+  }
+
+  if (isValid) {
+    // Redirect to home page or perform any other action
+    window.location.href = 'home.html';
+  }
+}
+
+
 
