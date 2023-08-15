3eb741038bd084c3a099777bdbeef0418e8337f2
Devansh Shah <devansh.shah@transact.box.com>
2023-08-15 16:11:11 -0400

    testimonials

--- js/home.js
+++ js/home.js
@@ -1,3 +1,21 @@
+let card = document.querySelectorAll(".card");
+let contentBox = document.querySelectorAll(".contentBox");
+
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("mouseover", function () {
+        for (let i = 0; i < contentBox.length; i++) {
+            contentBox[i].className = "contentBox";
+        }
+        document.getElementById(this.dataset.id).className = "contentBox active";
+
+        for (let i = 0; i < card.length; i++) {
+            card[i].className = "card";
+        }
+        this.className = "card active";
+    });
+}
+
+
 // Vegas Slider
 
 $(document).ready(function () {
